19c1100150874eec86ffdd95e13bd9f5e89fa09c
Lize Deirino <lize.deirino@gmail.com>
2025-02-28 15:04:31 +0200

    Version 2.0 Product Master view

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -12,7 +12,15 @@ export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
   private categoryUrl = 'http://localhost:8080/api/product-category'
 
+
   constructor(private httpClient: HttpClient) { }
+
+
+  getProduct(theProductId: number): Observable<Product> {
+    // build url based on product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    return this.httpClient.get<Product>(productUrl);
+  }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
     // build url based on category id
